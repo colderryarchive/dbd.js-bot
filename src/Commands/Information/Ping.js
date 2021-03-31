94afc3d574fa10b5216e13c5b3f66a61c3323c60
Colderry <72723052+Colderry@users.noreply.github.com>
2021-04-01 02:51:43 +0600

    Information: Ping.js

---
+++ src/Commands/Information/Ping.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: "ping",
+  code: 
+  `
+  $title[Pong!]
+  $description[$botPing MS]
+  $color[BLUE]
+  $cooldown[10s;
+    Command is on a \`%time%\` cooldown!
+    ]
+  `
+}
