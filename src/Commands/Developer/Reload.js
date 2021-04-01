0ff238ff7be42d74fe0082aaf88c126fc4c30505
Colderry <72723052+Colderry@users.noreply.github.com>
2021-04-02 00:31:58 +0600

    Command: Reload.js

---
+++ src/Commands/Developer/Reload.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: "reload",
+  code: 
+  `
+  $title[Reloading]
+  $color[BLUE]
+  $description[Loading commands]
+  $editIn[5s;{title: Reloaded}{description: **All Commands reloaded**}{color: #42BBF4}]
+  $updateCommands
+  $onlyForIDs[538627730921619486;{description: Developer only!}{color: #F65B5B}]
+  `
+}
