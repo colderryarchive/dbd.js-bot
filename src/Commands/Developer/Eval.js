9e42ff00e92bc7b78bd08667c5fe30ba46623e1d
Colderry <72723052+Colderry@users.noreply.github.com>
2021-04-02 00:16:52 +0600

    Command: Eval.js

---
+++ src/Commands/Developer/Eval.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: "eval",
+  code:
+    `
+  $eval[$message]
+  $onlyIf[$message!=;Give a code to execute]
+  $onlyForIDs[538627730921619486;419958345487745035;{description: Developer only!}{color: #F65B5B}]
+  `
+}
