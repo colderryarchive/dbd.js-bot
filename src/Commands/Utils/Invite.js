f0af09d37e2c565f08272c9bf83548474400e37b
Colderry <72723052+Colderry@users.noreply.github.com>
2021-04-19 21:04:14 +0600

    Command: Invite.js

---
+++ src/Commands/Utils/Invite.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: "invite",
+  code: 
+  `
+  $color[BLUE]
+  $title[Invite]
+  $thumbnail[$replaceText[$userAvatar[$findUser[823989416309293077]];size=2048;size=1024]]
+  $addField[Support;[Click Here](https://discord.gg/f6Ky3QUvUF)]
+  $addField[Invite;[Click Here](https://discord.com/oauth2/authorize?client_id=823989416309293077&scope=bot&permissions=8)]
+  `
+}
